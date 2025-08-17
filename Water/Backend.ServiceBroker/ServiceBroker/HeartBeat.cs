3de1c277e9586b2cad85408d73161dd72ee8b5d1
Joe Lenton <jlenton@me.com>
2025-08-18 00:28:26 +0100

    ServiceBroker pt 1

---
+++ Water/Backend.ServiceBroker/ServiceBroker/HeartBeat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.ServiceBroker.ServiceBroker
+{
+    internal class HeartBeat
+    {
+        private readonly TimeSpan tickInterval = TimeSpan.FromSeconds(1);
+        Processes processes;
+
+        HeartBeat(TimeSpan tickInterval, Processes processes)
+        {
+            this.tickInterval = tickInterval;
+            this.processes = processes;
+        }
+
+        public async Task RunAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine("Checking for Tasks");
+                var tasks = await processes.GetQueueTasks();
+                foreach (var task in tasks)
+                {
+                    try
+                    {
+                        processes.ExecuteTask(task);
+                        processes.MarkTaskComplete(task);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        processes.MarkTaskFailed(task);
+                    }
+                }
+ 
+                await Task.Delay(tickInterval, cancellationToken);
+            }
+        }
+    }
+}
---
+++ Water/Backend.ServiceBroker/ServiceBroker/Processes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.Core.EntityObjects;
+
+namespace Backend.ServiceBroker.ServiceBroker
+{
+    internal class Processes
+    {
+        internal async Task<List<TaskEO>> GetQueueTasks()
+        {
+            return new List<TaskEO>();
+        }
+
+        internal void ExecuteTask(TaskEO task)
+        {
+
+        }
+
+        internal bool MarkTaskComplete(TaskEO task)
+        {
+            return false;
+        }
+
+        internal bool MarkTaskFailed(TaskEO task)
+        {
+            return false;
+        }
+    }
+}
